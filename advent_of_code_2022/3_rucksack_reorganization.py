641f7eade16e794185d68fba3712eebbd3b4f810
nate meech <nate.meech@os.uk>
2022-12-16 16:27:12 +0000

    Updated day 3 with second half of solution.

--- advent_of_code_2022/3_rucksack_reorganization.py
+++ advent_of_code_2022/3_rucksack_reorganization.py
@@ -15,30 +15,53 @@ for num in range(0, 26):
     let_val[letters[num]] = count
     count += 1
 
-#split out two parts of rucksack
-ruck1 = []
-ruck2 = []
-for line in input:
-    line = line.strip()
-    ruck1.append(line[:len(line) // 2])
-    ruck2.append(line[len(line) // 2:])
+#part 1
+# #split out two parts of rucksack
+# ruck1 = []
+# ruck2 = []
+# for line in input:
+#     line = line.strip()
+#     ruck1.append(line[:len(line) // 2])
+#     ruck2.append(line[len(line) // 2:])
 
-#find duplicate value for each rucksack
-duplicates = [] 
-count2 = 0
-for string in ruck1:
-    for let in string:
-        if let in ruck2[count2]:
-            duplicates.append(let)
-            break
-    count2 += 1
+# #find duplicate value for each rucksack
+# duplicates = [] 
+# count2 = 0
+# for string in ruck1:
+#     for let in string:
+#         if let in ruck2[count2]:
+#             duplicates.append(let)
+#             break
+#     count2 += 1
 
-#check correct number of duplicates - instructions say 1 per rucksack
-print("Correct number of duplicates." if len(duplicates) == len(ruck1) else "Incorrect number of duplicates.")
+# #check correct number of duplicates - instructions say 1 per rucksack
+# print("Correct number of duplicates." if len(duplicates) == len(ruck1) else "Incorrect number of duplicates.")
 
-#sum duplicates
+# #sum duplicates
+# total = 0
+# for dup in duplicates: total += let_val[dup]
+
+# #print answer
+# print(total)
+
+#part 2
+#split groups and find common letters (badges)
+badges = []
+lines = input.readlines()
+count = 0
+while count < len(lines) - 2:
+    group = []
+    for line in lines[count:count + 3]:
+        group.append(set(line.strip()))
+    badges.append(" ".join(map(str, set.intersection(group[0], group[1], group[2]))))
+    count += 3
+
+#check correct number of badges - instructions say 1 per 3 elves
+print("Correct number of badges." if len(badges) == len(lines) // 3 else "Incorrect number of badges.")
+
+#sum badges
 total = 0
-for dup in duplicates: total += let_val[dup]
+for badge in badges: total += let_val[badge]
 
 #print answer
 print(total)
